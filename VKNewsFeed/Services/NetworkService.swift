3a2cf32293622baf98e9464a4b069bce6fb84ea4
Yaroslav <Yaraslav.IOS.Developer@gmail.com>
2021-04-22 16:18:28 +0300

    Fetch news feed JSON Data

--- VKNewsFeed/Services/NetworkService.swift
+++ VKNewsFeed/Services/NetworkService.swift
@@ -8,7 +8,12 @@
 import Foundation
 
 
-class NetworkService {
+protocol Networking {
+    func request(path: String, params: [String: String], completion: @escaping (Data?, Error?) -> Void)
+}
+class NetworkService: Networking {
+    
+    
     
     private let authorizationService: AuthorizationService
     
@@ -16,24 +21,36 @@ class NetworkService {
         self.authorizationService = authorizationService
     }
     
-    func getFeed() {
-        
-        var components = URLComponents()
-        
-        //https://api.vk.com/method/users.get?user_ids=210700286&fields=bdate&access_token=533bacf01e11f55b536a565b57531ac114461ae8736d6506a3&v=5.130
-        
+    func request(path: String, params: [String : String], completion: @escaping (Data?, Error?) -> Void) {
         guard let token = authorizationService.token else { return }
         let params = ["filters": "post, photo"]
         var allparams = params
         allparams["access_token"] = token
         allparams["v"] = API.versiom
+        let url  = self.url(from: path, params: allparams)
+        let requst = URLRequest(url: url)
+        let task = createDataTask(from: requst, completion: completion)
         
+        task.resume()
+        print(url)
+    }
+   
+    private func createDataTask(from requst: URLRequest, completion: @escaping (Data?, Error?) -> Void) -> URLSessionDataTask {
+        
+        return URLSession.shared.dataTask(with: requst) { (data, response, error) in
+            DispatchQueue.main.async {
+                completion(data, error)
+            }
+        }
+    }
+    
+    private func url(from path: String, params: [String: String]) -> URL {
+        var components = URLComponents()
         components.scheme = API.scheme
         components.host = API.host
         components.path = API.newsFeed
-        components.queryItems = allparams.map{ URLQueryItem(name: $0, value: $1) }
+        components.queryItems = params.map{ URLQueryItem(name: $0, value: $1) }
         
-        guard let url = components.url else { return }
-        print(url)
+        return components.url!
     }
 }
